bf7c3f494d47998492ddcadcfee811e336d71b82
berkaybaltaci <berkaybaltaci@gmail.com>
2023-03-30 11:42:45 +0100

    Initial Commit

---
+++ PongChallenge/PongChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongChallengeApp.swift
+//  PongChallenge
+//
+//  Created by Berkay Baltaci on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PongChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
